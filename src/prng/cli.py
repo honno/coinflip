2201816d9091c05c89256f6dd0ce70adfbe73149
Matthew Barber <quitesimplymatt@gmail.com>
2020-04-23 09:51:34 +0100

    serialising porcelain

--- src/prng/cli.py
+++ src/prng/cli.py
@@ -1,7 +1,21 @@
 import click
 
+READ_FILE_TYPE = click.File(mode='r', encoding='utf-8')
 
-@click.command()
-@click.argument('names', nargs=-1)
-def main(names):
-    click.echo(repr(names))
+
+@click.group()
+def main():
+    pass
+
+
+@main.command()
+@click.argument('data', type=READ_FILE_TYPE)
+@click.argument('spec', type=READ_FILE_TYPE)
+@click.option('-o', '--overwrite', is_flag=True)
+def serialize(data, spec, overwrite=False):
+    pass
+
+
+@main.command()
+def ls():
+    pass
