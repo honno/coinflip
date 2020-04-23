a799a22a479b49801a829cdf401ab381ac304bfd
Matthew Barber <quitesimplymatt@gmail.com>
2020-04-23 20:05:09 +0100

    working basic store functionality

--- setup.py
+++ setup.py
@@ -75,6 +75,7 @@ setup(
     install_requires=[
         'click',
         'toml',
+        'numpy',
     ],
     test_requires=[
         'pyfakefs',
--- src/prng/cli.py
+++ src/prng/cli.py
@@ -1,5 +1,8 @@
 import click
 
+from prng.store import manifest_keys
+from prng.store import store
+
 
 @click.group()
 def main():
@@ -7,13 +10,14 @@ def main():
 
 
 @main.command()
-@click.argument('data', type=click.File('r'))
-@click.argument('spec', type=click.File('r'))
-@click.option('-o', '--overwrite', is_flag=True)
+@click.argument("data", type=click.File("r"))
+@click.argument("spec", type=click.File("r"))
+@click.option("-o", "--overwrite", is_flag=True)
 def serialize(data, spec, overwrite=False):
-    click.echo(spec.read())
+    store(data, spec, overwrite)
 
 
 @main.command()
 def ls():
-    pass
+    for store_name in manifest_keys():
+        click.echo(store_name)
