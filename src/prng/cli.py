b04488c418498103b80c472bfc7c72a590e6c6b3
Matthew Barber <quitesimplymatt@gmail.com>
2020-04-23 09:44:51 +0100

    remove docstring

--- src/prng/cli.py
+++ src/prng/cli.py
@@ -1,19 +1,3 @@
-"""
-Module that contains the command line app.
-
-Why does this file exist, and why not put this in __main__?
-
-  You might be tempted to import things from __main__ later, but that will cause
-  problems: the code will get executed twice:
-
-  - When you run `python -m prng` python will execute
-    ``__main__.py`` as a script. That means there won't be any
-    ``prng.__main__`` in ``sys.modules``.
-  - When you import __main__ it will get executed again (as a module) because
-    there's no ``prng.__main__`` in ``sys.modules``.
-
-  Also see (1) from http://click.pocoo.org/5/setuptools/#setuptools-integration
-"""
 import click
 
 
