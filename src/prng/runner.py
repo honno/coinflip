35d7faed7a92a531fdbbdb41848ef704d74792fd
Your Name <quitesimplymatt@gmail.com>
2020-04-29 16:58:56 +0100

    frequency in block test stub

--- src/prng/runner.py
+++ src/prng/runner.py
@@ -1,7 +1,9 @@
 from prng.stattests.frequency import frequency
+from prng.stattests.frequency import frequency_in_block
 
 __all__ = ["run_tests"]
 
 
 def run_tests(s):
     frequency(s)
+    frequency_in_block(s)
--- src/prng/stattests/frequency.py
+++ src/prng/stattests/frequency.py
@@ -1,7 +1,23 @@
+from math import ceil
+
 from click import echo
 
 
 def frequency(series):
     counts = series.value_counts()
 
-    echo(counts)
+    echo(counts.head())
+
+    # TODO summary statistics
+
+
+def frequency_in_block(series, block_size=None, nblocks=10):
+    if block_size is None:
+        block_size = ceil(len(series) / nblocks)
+
+    while len(series) != 0:
+        series_block, series = series[:block_size], series[block_size:]
+
+        frequency(series_block)
+
+    # TODO summary statistics
