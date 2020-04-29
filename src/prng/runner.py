9e35b10265bbd687138ff9e14784c0b80bab630e
Your Name <quitesimplymatt@gmail.com>
2020-04-29 18:59:33 +0100

    runs_in_block stub

--- src/prng/runner.py
+++ src/prng/runner.py
@@ -1,6 +1,7 @@
 from prng.stattests.frequency import frequency
 from prng.stattests.frequency import frequency_in_block
 from prng.stattests.runs import runs
+from prng.stattests.runs import runs_in_block
 
 __all__ = ["run_tests"]
 
@@ -9,3 +10,4 @@ def run_tests(s):
     frequency(s)
     frequency_in_block(s)
     runs(s)
+    runs_in_block(s)
