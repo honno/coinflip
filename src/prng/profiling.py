1bd350a4a58281e4bdca02ff6df10b3b8c7015db
Your Name <quitesimplymatt@gmail.com>
2020-05-01 12:57:51 +0100

    multi profile decorator

--- src/prng/profiling.py
+++ src/prng/profiling.py
@@ -4,6 +4,7 @@ from importlib.util import module_from_spec
 from importlib.util import spec_from_file_location
 
 PROFILE_KEY = "is_profile"
+MULTI_PROFILE_KEY = "is_multi_profile"
 
 
 def profile(func):
@@ -12,19 +13,47 @@ def profile(func):
         name = func.__name__
         series = func(df)
 
+        series = series.rename(name)
+
         return name, series
 
-    # TODO is some type hacking preferred here?
     setattr(wrapper, PROFILE_KEY, True)
 
     return wrapper
 
 
+def multi_profile(func):
+    @wraps(func)
+    def wrapper(df):
+        name = func.__name__
+        multiple_series = func(df)
+
+        for i, series in enumerate(multiple_series):
+            profile_name = name + f"_{i}"
+
+            series = series.rename(profile_name)
+
+            yield profile_name, series
+
+    setattr(wrapper, MULTI_PROFILE_KEY, True)
+
+    return wrapper
+
+
 def profile_functions(functions):
-    for function in functions:
+    for func in functions:
         try:
-            if getattr(function, PROFILE_KEY):
-                yield function
+            if getattr(func, PROFILE_KEY):
+                yield func
+        except AttributeError:
+            pass
+
+
+def multi_profile_functions(functions):
+    for func in functions:
+        try:
+            if getattr(func, MULTI_PROFILE_KEY):
+                yield func
         except AttributeError:
             pass
 
@@ -43,3 +72,10 @@ def profiled_data(df, profiles_path):
         name, series = profile(df)
 
         yield name, series
+
+    multi_profiles = multi_profile_functions(functions)
+    for func in multi_profiles:
+        profiles = func(df)
+        for name, series in profiles:
+
+            yield name, series
