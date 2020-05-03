1c994bb95a83b8ba2eacf0c1a1021bf1e3902c01
Matthew Barber <quitesimplymatt@gmail.com>
2020-05-03 18:09:35 +0100

    added hypothesis

--- tests/test_frequency.py
+++ tests/test_frequency.py
@@ -1,16 +1,30 @@
 from math import isclose
 
 import pandas as pd
+from hypothesis import given
+from hypothesis import strategies as st
 
 from rngtest.stattests import frequency
 
 from .implementations import dj
 
 
-def test_nist_example():
-    sequence = [1, 0, 1, 1, 0, 1, 0, 1, 0, 1]
+def contains_multiple_values(array):
+    first_value = array[0]
+    for value in array[1:]:
+        if value != first_value:
+            return True
+    else:
+        return False
 
-    our_result = frequency.frequency(pd.Series(sequence))
-    dj_result = dj.monobit_test(sequence)
+
+@given(
+    st.lists(st.integers(min_value=0, max_value=1), min_size=2).filter(
+        contains_multiple_values
+    )
+)
+def test_nist_example(bits):
+    our_result = frequency.frequency(pd.Series(bits))
+    dj_result = dj.monobit_test(bits)
 
     assert isclose(our_result.p, dj_result.p)
