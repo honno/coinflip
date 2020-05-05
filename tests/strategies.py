82c73ba527317512843118e549bc784929fd4312
Matthew Barber <quitesimplymatt@gmail.com>
2020-05-05 12:52:01 +0100

    seperate strategies module

---
+++ tests/strategies.py
@@ -0,0 +1,17 @@
+from hypothesis import strategies as st
+
+__all__ = ["random_bits_strategy"]
+
+
+def contains_multiple_values(array):
+    first_value = array[0]
+    for value in array[1:]:
+        if value != first_value:
+            return True
+    else:
+        return False
+
+
+binary = st.integers(min_value=0, max_value=1)
+bits = st.lists(binary, min_size=2)
+random_bits_strategy = bits.filter(contains_multiple_values)
--- tests/test_frequency.py
+++ tests/test_frequency.py
@@ -2,27 +2,14 @@ from math import isclose
 
 import pandas as pd
 from hypothesis import given
-from hypothesis import strategies as st
 
 from rngtest.stattests import frequency
 
 from .implementations import dj
+from .strategies import random_bits_strategy
 
 
-def contains_multiple_values(array):
-    first_value = array[0]
-    for value in array[1:]:
-        if value != first_value:
-            return True
-    else:
-        return False
-
-
-@given(
-    st.lists(st.integers(min_value=0, max_value=1), min_size=2).filter(
-        contains_multiple_values
-    )
-)
+@given(random_bits_strategy)
 def test_nist_example(bits):
     our_result = frequency.frequency(pd.Series(bits))
     dj_result = dj.monobit_test(bits)
