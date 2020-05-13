b402ef97a66a97ee9a1b2a5df392bb8c5df3d711
Matthew Barber <quitesimplymatt@gmail.com>
2020-05-13 09:32:03 +0100

    slugify documentation

--- src/rngtest/slugify.py
+++ src/rngtest/slugify.py
@@ -1,12 +1,16 @@
+"""Module to contain a standard slugify method"""
 import re
 
-from slugify import slugify as slugify_
+from slugify import slugify as _slugify
+
+__all_ = ["slugify"]
 
 r_underscore = re.compile("_")
 
 
 def slugify(text: str) -> str:
+    """Slugify the string with underscore seperators if present"""
     if r_underscore.search(text):
-        return slugify_(text, separator="_")
+        return _slugify(text, separator="_")
     else:
-        return slugify_(text)
+        return _slugify(text)
