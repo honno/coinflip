8810a2069cb185b22a3f9d7b7d487ac6c1839155
Matthew Barber <quitesimplymatt@gmail.com>
2020-05-25 15:40:35 +0100

    compared fourier test examples with dj
    There's a mismatch!

---
+++ tests/test_implementations_on_examples.py
@@ -0,0 +1,36 @@
+# fmt: off
+from math import isclose
+
+from .implementations import dj
+
+
+def test_discrete_fourier_transform_small():
+    bits = [1, 0, 0, 1, 0, 1, 0, 0, 1, 1]
+
+    dj_result = dj.fourier_test(bits)
+    nist_p = 0.029523
+
+    assert isclose(dj_result.p, nist_p, abs_tol=0.005)
+
+
+def test_discrete_fourier_transform_large():
+    bits = [
+        1, 1, 0, 0, 1, 0, 0, 1,
+        0, 0, 0, 0, 1, 1, 1, 1,
+        1, 1, 0, 1, 1, 0, 1, 0,
+        1, 0, 1, 0, 0, 0, 1, 0,
+        0, 0, 1, 0, 0, 0, 0, 1,
+        0, 1, 1, 0, 1, 0, 0, 0,
+        1, 1, 0, 0, 0, 0, 1, 0,
+        0, 0, 1, 1, 0, 1, 0, 0,
+        1, 1, 0, 0, 0, 1, 0, 0,
+        1, 1, 0, 0, 0, 1, 1, 0,
+        0, 1, 1, 0, 0, 0, 1, 0,
+        1, 0, 0, 0, 1, 0, 1, 1,
+        1, 0, 0, 0,
+    ]
+
+    dj_result = dj.fourier_test(bits)
+    nist_p = 0.168669
+
+    assert isclose(dj_result.p, nist_p, abs_tol=0.005)
