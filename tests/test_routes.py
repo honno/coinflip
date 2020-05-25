ff0809d8aa5753d9c1af9db2ae940126a30d8618
Matthew Barber <quitesimplymatt@gmail.com>
2020-05-25 09:45:45 +0100

    restricted examples and state counts for cli routes test
    So it can actually run in CI. Needs to be revisited!

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,4 +1,5 @@
 import re
+from shutil import rmtree
 from tempfile import NamedTemporaryFile
 
 from click.testing import CliRunner
@@ -7,14 +8,22 @@ from hypothesis.stateful import Bundle
 from hypothesis.stateful import RuleBasedStateMachine
 from hypothesis.stateful import consumes
 from hypothesis.stateful import rule
+from pytest import fixture
 
 from rngtest import cli
+from rngtest.store import data_dir
 
 from .strategies import random_bits_strategy
 
 r_storename = re.compile(
     "Store name to be encoded as ([a-z0-9]+)\n", flags=re.IGNORECASE
 )
+
+
+@fixture(autouse=True, scope="module")
+def module_setup_teardown():
+    yield
+    rmtree(data_dir)
 
 
 class CliRoutes(RuleBasedStateMachine):
@@ -36,31 +45,26 @@ class CliRoutes(RuleBasedStateMachine):
             f.seek(0)
             result = self.cli.invoke(cli.load, [f.name])
 
-        storename_msg = r_storename.search(result.stdout)
-        storename = storename_msg.group(1)
+        store_name_msg = r_storename.search(result.stdout)
+        store_name = store_name_msg.group(1)
 
-        return storename
+        return store_name
 
-    @rule(name=storenames)
-    def find_storename_listed(self, name):
+    @rule(store_name=storenames)
+    def find_storename_listed(self, store_name):
         result = self.cli.invoke(cli.ls)
-        assert re.search(name, result.stdout)
+        assert re.search(store_name, result.stdout)
 
-    @rule(name=consumes(storenames))
-    def remove_store(self, name):
-        rm_result = self.cli.invoke(cli.rm, [name])
+    @rule(store_name=consumes(storenames))
+    def remove_store(self, store_name):
+        rm_result = self.cli.invoke(cli.rm, [store_name])
         assert rm_result.exit_code == 0
 
         ls_result = self.cli.invoke(cli.ls)
-        assert not re.search(name, ls_result.stdout)
-
-    def teardown(self):
-        pass
+        assert not re.search(store_name, ls_result.stdout)
 
 
-CliRoutes.TestCase.settings = settings(
-    max_examples=1  # TODO work out sensible examples no.
-)
+CliRoutes.TestCase.settings = settings(max_examples=1, stateful_step_count=1)
 
 
 TestStore = CliRoutes.TestCase
