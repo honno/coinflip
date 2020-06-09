ef5e17504f69b06c71aacb8c753cb511e1c0dfad
Matthew Barber <quitesimplymatt@gmail.com>
2020-06-09 10:46:39 +0100

    better naming for fourier test

--- src/rngtest/stattests/fourier.py
+++ src/rngtest/stattests/fourier.py
@@ -32,23 +32,23 @@ def discrete_fourier_transform(series, candidate):
     oscillations = series.map({peaks: 1, trough: -1})
     fourier = fft(oscillations)
 
-    fourier_first_half = fourier[: n // 2]
-    peaks = fourier_first_half.abs()
+    halffourier = fourier[: n // 2]
+    peaks = halffourier.abs()
 
     threshold = sqrt(log(1 / 0.05) * n)
-    expected_below_threshold = 0.95 * n / 2
-    actual_below_threshold = sum(peaks < threshold)  # TODO not accurate
+    expectedbelow = 0.95 * n / 2
+    actualbelow = sum(peaks < threshold)  # TODO not accurate
 
-    difference = actual_below_threshold - expected_below_threshold
-    normalised_diff = difference / sqrt((n * 0.95 * 0.05) / 4)
+    diff = actualbelow - expectedbelow
+    normdiff = diff / sqrt((n * 0.95 * 0.05) / 4)
 
-    p = erfc(abs(normalised_diff) / sqrt(2))
+    p = erfc(abs(normdiff) / sqrt(2))
 
-    return TestResult(statistic=normalised_diff, p=p)
+    return TestResult(statistic=normdiff, p=p)
 
 
-def fft(oscillations):
-    fourierndarray = _fft(oscillations)
-    fourierseries = pd.Series(fourierndarray)
+def fft(array) -> pd.Series:
+    fourier_ndarray = _fft(array)
+    fourier_series = pd.Series(fourier_ndarray)
 
-    return fourierseries
+    return fourier_series
