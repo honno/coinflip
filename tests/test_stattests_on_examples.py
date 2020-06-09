aae7ea95ae860f31f7e68ae5159479e2c250a20f
Matthew Barber <quitesimplymatt@gmail.com>
2020-06-09 16:30:54 +0100

    namedtuple defines examples and passed to parameterized test

--- tests/test_stattests_on_examples.py
+++ tests/test_stattests_on_examples.py
@@ -1,10 +1,14 @@
-# fmt: off
 from math import isclose
 from pathlib import Path
+from typing import Any
+from typing import Callable
+from typing import Dict
+from typing import List
 from typing import NamedTuple
 from typing import Union
 
 import pandas as pd
+import pytest
 
 from rngtest.stattests import complexity
 from rngtest.stattests import fourier
@@ -13,234 +17,6 @@ from rngtest.stattests import matrix
 from rngtest.stattests import runs
 from rngtest.stattests import template
 from rngtest.stattests import universal
-
-
-class StattestResult(NamedTuple):
-    statistic: int
-    p: Union[int, float]
-
-
-def example(bits, statistic, p, stattest, **kwargs):
-    def decorator(testing_function):
-        def wrapper():
-            series = pd.Series(bits)
-            our_result = stattest(series, **kwargs)
-
-            example_result = StattestResult(statistic=statistic, p=p)
-
-            testing_function(our_result, example_result)
-
-        return wrapper
-
-    return decorator
-
-
-@example(
-    stattest=frequency.monobits,
-
-    bits=[1, 0, 1, 1, 0, 1, 0, 1, 0, 1],
-
-    statistic=.632455532,
-    p=0.527089,
-)
-def test_monobits_small(our_result, nist_result):
-    assert isclose(our_result.statistic, nist_result.statistic, abs_tol=0.05)
-    assert isclose(our_result.p, nist_result.p, abs_tol=0.005)
-
-
-@example(
-    stattest=frequency.frequency_within_block,
-
-    bits=[
-        1, 1, 0, 0, 1, 0, 0, 1,
-        0, 0, 0, 0, 1, 1, 1, 1,
-        1, 1, 0, 1, 1, 0, 1, 0,
-        1, 0, 1, 0, 0, 0, 1, 0,
-        0, 0, 1, 0, 0, 0, 0, 1,
-        0, 1, 1, 0, 1, 0, 0, 0,
-        1, 1, 0, 0, 0, 0, 1, 0,
-        0, 0, 1, 1, 0, 1, 0, 0,
-        1, 1, 0, 0, 0, 1, 0, 0,
-        1, 1, 0, 0, 0, 1, 1, 0,
-        0, 1, 1, 0, 0, 0, 1, 0,
-        1, 0, 0, 0, 1, 0, 1, 1,
-        1, 0, 0, 0,
-    ],
-    blocksize=10,
-
-    statistic=7.2,
-    p=0.706438,
-)
-def test_frequency_within_block_large(our_result, nist_result):
-    assert isclose(our_result.statistic, nist_result.statistic, abs_tol=0.05)
-    assert isclose(our_result.p, nist_result.p, abs_tol=0.005)
-
-
-@example(
-    stattest=runs.runs,
-
-    bits=[
-        1, 0, 0, 1, 1, 0, 1, 0,
-        1, 1
-    ],
-
-    statistic=7,
-    p=0.147232,
-)
-def test_runs_small(our_result, nist_result):
-    assert our_result.statistic == nist_result.statistic
-    assert isclose(our_result.p, nist_result.p, abs_tol=0.005)
-
-
-@example(
-    stattest=runs.longest_runs,
-
-    bits=[
-        1, 1, 0, 0, 1, 1, 0, 0,
-        0, 0, 0, 1, 0, 1, 0, 1,
-        0, 1, 1, 0, 1, 1, 0, 0,
-        0, 1, 0, 0, 1, 1, 0, 0,
-        1, 1, 1, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 1, 0,
-        0, 1, 0, 0, 1, 1, 0, 1,
-        0, 1, 0, 1, 0, 0, 0, 1,
-        0, 0, 0, 1, 0, 0, 1, 1,
-        1, 1, 0, 1, 0, 1, 1, 0,
-        1, 0, 0, 0, 0, 0, 0, 0,
-        1, 1, 0, 1, 0, 1, 1, 1,
-        1, 1, 0, 0, 1, 1, 0, 0,
-        1, 1, 1, 0, 0, 1, 1, 0,
-        1, 1, 0, 1, 1, 0, 0, 0,
-        1, 0, 1, 1, 0, 0, 1, 0,
-    ],
-
-    statistic=4.882605,
-    p=0.180609,
-)
-def test_longest_runs_large(our_result, nist_result):
-    assert isclose(our_result.statistic, nist_result.statistic, abs_tol=0.005)
-    assert isclose(our_result.p, nist_result.p, abs_tol=0.005)
-
-
-@example(
-    stattest=matrix.binary_matrix_rank,
-
-    bits=[
-        0, 1, 0, 1, 1, 0, 0, 1,
-        0, 0, 1, 0, 1, 0, 1, 0,
-        1, 1, 0, 1,
-    ],
-    nrows=3,
-    ncols=3,
-
-    statistic=0.596953,
-    p=0.741948,
-)
-def test_binary_matrix_rank_small(our_result, nist_result):
-    assert isclose(our_result.statistic, nist_result.statistic, abs_tol=0.005)
-    assert isclose(our_result.p, nist_result.p, abs_tol=0.005)
-
-
-@example(
-    stattest=fourier.discrete_fourier_transform,
-
-    bits=[1, 0, 0, 1, 0, 1, 0, 0, 1, 1],
-
-    statistic=-2.176429,
-    p=0.029523,
-)
-def test_discrete_fourier_transform_small(our_result, nist_result):
-    assert isclose(our_result.statistic, nist_result.statistic, abs_tol=0.005)
-    assert isclose(our_result.p, nist_result.p, abs_tol=0.005)
-
-
-@example(
-    stattest=fourier.discrete_fourier_transform,
-
-    bits=[
-        1, 1, 0, 0, 1, 0, 0, 1,
-        0, 0, 0, 0, 1, 1, 1, 1,
-        1, 1, 0, 1, 1, 0, 1, 0,
-        1, 0, 1, 0, 0, 0, 1, 0,
-        0, 0, 1, 0, 0, 0, 0, 1,
-        0, 1, 1, 0, 1, 0, 0, 0,
-        1, 1, 0, 0, 0, 0, 1, 0,
-        0, 0, 1, 1, 0, 1, 0, 0,
-        1, 1, 0, 0, 0, 1, 0, 0,
-        1, 1, 0, 0, 0, 1, 1, 0,
-        0, 1, 1, 0, 0, 0, 1, 0,
-        1, 0, 0, 0, 1, 0, 1, 1,
-        1, 0, 0, 0,
-    ],
-
-    statistic=-1.376494,
-    p=0.168669,
-)
-def test_discrete_fourier_transform_large(our_result, nist_result):
-    assert isclose(our_result.statistic, nist_result.statistic, abs_tol=0.005)
-    assert isclose(our_result.p, nist_result.p, abs_tol=0.005)
-
-
-@example(
-    stattest=template.non_overlapping_template_matching,
-
-    bits=[
-        1, 0, 1, 0, 0, 1, 0, 0,
-        1, 0, 1, 1, 1, 0, 0, 1,
-        0, 1, 1, 0
-    ],
-    template=pd.Series([0, 0, 1]),
-    nblocks=2,
-
-    statistic=2.133333,
-    p=0.344154,
-)
-def test_non_overlapping_template_matching_small(our_result, nist_result):
-    assert isclose(our_result.statistic, nist_result.statistic, abs_tol=0.005)
-    assert isclose(our_result.p, nist_result.p, abs_tol=0.005)
-
-
-@example(
-    stattest=template.overlapping_template_matching,
-
-    bits=[
-        1, 0, 1, 1, 1, 0, 1, 1,
-        1, 1, 0, 0, 1, 0, 1, 1,
-        0, 1, 0, 0, 0, 1, 1, 1,
-        0, 0, 1, 0, 1, 1, 1, 0,
-        1, 1, 1, 1, 1, 0, 0, 0,
-        0, 1, 0, 1, 1, 0, 1, 0,
-        0, 1,
-    ],
-    template=pd.Series([1, 1]),
-    nblocks=5,
-
-    statistic=3.167729,
-    p=0.274932,
-)
-def test_overlapping_template_matching_small(our_result, nist_result):
-    assert isclose(our_result.statistic, nist_result.statistic, abs_tol=0.005)
-    assert isclose(our_result.p, nist_result.p, abs_tol=0.005)
-
-
-@example(
-    stattest=universal.maurers_universal,
-
-    bits=[
-        0, 1, 0, 1, 1, 0, 1, 0,
-        0, 1, 1, 1, 0, 1, 0, 1,
-        0, 1, 1, 1,
-    ],
-    blocksize=2,
-    init_nblocks=4,
-
-    statistic=1.1949875,
-    p=0.767189,
-)
-def test_maurers_universal(our_result, nist_result):
-    assert isclose(our_result.statistic, nist_result.statistic, abs_tol=0.005)
-    assert isclose(our_result.p, nist_result.p, abs_tol=0.005)
-
 
 tests_path = Path(__file__).parent
 data_path = tests_path / "data"
@@ -256,15 +32,211 @@ def e_expansion():
                     yield bit
 
 
-@example(
-    stattest=complexity.linear_complexity,
+class Example(NamedTuple):
+    stattest: Callable
+    bits: List[int]
+    statistic: Union[int, float]
+    p: float
+    kwargs: Dict[str, Any] = dict()
 
-    bits=e_expansion(),
-    blocksize=1000,
 
-    statistic=2.700348,
-    p=0.845406,
-)
-def test_linear_complexity(our_result, nist_result):
-    assert isclose(our_result.statistic, nist_result.statistic, abs_tol=0.005)
-    assert isclose(our_result.p, nist_result.p, abs_tol=0.005)
+# fmt: off
+examples = [
+    Example(
+        stattest=frequency.monobits,
+
+        bits=[1, 0, 1, 1, 0, 1, 0, 1, 0, 1],
+
+        statistic=.632455532,
+        p=0.527089,
+    ),
+    Example(
+        stattest=frequency.monobits,
+
+        bits=[1, 0, 1, 1, 0, 1, 0, 1, 0, 1],
+
+        statistic=.632455532,
+        p=0.527089,
+    ),
+    Example(
+        stattest=frequency.frequency_within_block,
+
+        bits=[
+            1, 1, 0, 0, 1, 0, 0, 1,
+            0, 0, 0, 0, 1, 1, 1, 1,
+            1, 1, 0, 1, 1, 0, 1, 0,
+            1, 0, 1, 0, 0, 0, 1, 0,
+            0, 0, 1, 0, 0, 0, 0, 1,
+            0, 1, 1, 0, 1, 0, 0, 0,
+            1, 1, 0, 0, 0, 0, 1, 0,
+            0, 0, 1, 1, 0, 1, 0, 0,
+            1, 1, 0, 0, 0, 1, 0, 0,
+            1, 1, 0, 0, 0, 1, 1, 0,
+            0, 1, 1, 0, 0, 0, 1, 0,
+            1, 0, 0, 0, 1, 0, 1, 1,
+            1, 0, 0, 0,
+        ],
+        kwargs={"blocksize": 10},
+
+        statistic=7.2,
+        p=0.706438,
+    ),
+    Example(
+        stattest=runs.runs,
+
+        bits=[
+            1, 0, 0, 1, 1, 0, 1, 0,
+            1, 1
+        ],
+
+        statistic=7,
+        p=0.147232,
+    ),
+    Example(
+        stattest=runs.longest_runs,
+
+        bits=[
+            1, 1, 0, 0, 1, 1, 0, 0,
+            0, 0, 0, 1, 0, 1, 0, 1,
+            0, 1, 1, 0, 1, 1, 0, 0,
+            0, 1, 0, 0, 1, 1, 0, 0,
+            1, 1, 1, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 1, 0,
+            0, 1, 0, 0, 1, 1, 0, 1,
+            0, 1, 0, 1, 0, 0, 0, 1,
+            0, 0, 0, 1, 0, 0, 1, 1,
+            1, 1, 0, 1, 0, 1, 1, 0,
+            1, 0, 0, 0, 0, 0, 0, 0,
+            1, 1, 0, 1, 0, 1, 1, 1,
+            1, 1, 0, 0, 1, 1, 0, 0,
+            1, 1, 1, 0, 0, 1, 1, 0,
+            1, 1, 0, 1, 1, 0, 0, 0,
+            1, 0, 1, 1, 0, 0, 1, 0,
+        ],
+
+
+        statistic=4.882605,
+        p=0.180609,
+    ),
+    Example(
+        stattest=matrix.binary_matrix_rank,
+
+        bits=[
+            0, 1, 0, 1, 1, 0, 0, 1,
+            0, 0, 1, 0, 1, 0, 1, 0,
+            1, 1, 0, 1,
+        ],
+        kwargs={
+            "nrows": 3,
+            "ncols": 3,
+        },
+
+        statistic=0.596953,
+        p=0.741948,
+    ),
+    Example(
+        stattest=fourier.discrete_fourier_transform,
+
+        bits=[1, 0, 0, 1, 0, 1, 0, 0, 1, 1],
+
+        statistic=-2.176429,
+        p=0.029523,
+    ),
+    Example(
+        stattest=fourier.discrete_fourier_transform,
+
+        bits=[
+            1, 1, 0, 0, 1, 0, 0, 1,
+            0, 0, 0, 0, 1, 1, 1, 1,
+            1, 1, 0, 1, 1, 0, 1, 0,
+            1, 0, 1, 0, 0, 0, 1, 0,
+            0, 0, 1, 0, 0, 0, 0, 1,
+            0, 1, 1, 0, 1, 0, 0, 0,
+            1, 1, 0, 0, 0, 0, 1, 0,
+            0, 0, 1, 1, 0, 1, 0, 0,
+            1, 1, 0, 0, 0, 1, 0, 0,
+            1, 1, 0, 0, 0, 1, 1, 0,
+            0, 1, 1, 0, 0, 0, 1, 0,
+            1, 0, 0, 0, 1, 0, 1, 1,
+            1, 0, 0, 0,
+        ],
+
+        statistic=-1.376494,
+        p=0.168669,
+    ),
+    Example(
+        stattest=template.non_overlapping_template_matching,
+
+        bits=[
+            1, 0, 1, 0, 0, 1, 0, 0,
+            1, 0, 1, 1, 1, 0, 0, 1,
+            0, 1, 1, 0
+        ],
+        kwargs={
+            "template": pd.Series([0, 0, 1]),
+            "nblocks": 2,
+        },
+
+        statistic=2.133333,
+        p=0.344154,
+    ),
+    Example(
+        stattest=template.overlapping_template_matching,
+
+        bits=[
+            1, 0, 1, 1, 1, 0, 1, 1,
+            1, 1, 0, 0, 1, 0, 1, 1,
+            0, 1, 0, 0, 0, 1, 1, 1,
+            0, 0, 1, 0, 1, 1, 1, 0,
+            1, 1, 1, 1, 1, 0, 0, 0,
+            0, 1, 0, 1, 1, 0, 1, 0,
+            0, 1,
+        ],
+        kwargs={
+            "template": pd.Series([1, 1]),
+            "nblocks": 5,
+        },
+
+        statistic=3.167729,
+        p=0.274932,
+    ),
+    Example(
+        stattest=universal.maurers_universal,
+
+        bits=[
+            0, 1, 0, 1, 1, 0, 1, 0,
+            0, 1, 1, 1, 0, 1, 0, 1,
+            0, 1, 1, 1,
+        ],
+        kwargs={
+            "blocksize": 2,
+            "init_nblocks": 4,
+        },
+
+        statistic=1.1949875,
+        p=0.767189,
+    ),
+    Example(
+        stattest=complexity.linear_complexity,
+
+        bits=e_expansion(),
+        kwargs={"blocksize": 1000},
+
+        statistic=2.700348,
+        p=0.845406,
+    ),
+]
+# fmt: on
+
+
+@pytest.mark.parametrize("stattest, bits, statistic, p, kwargs", examples)
+def test_stattest_on_example(stattest, bits, statistic, p, kwargs):
+    series = pd.Series(bits)
+    result = stattest(series, **kwargs)
+
+    if isinstance(statistic, float):
+        assert isclose(result.statistic, statistic, rel_tol=0.05)
+    elif isinstance(statistic, int):
+        assert result.statistic == statistic
+
+    assert isclose(result.p, p, abs_tol=0.005)
