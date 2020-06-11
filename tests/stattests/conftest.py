1e178a591d281e9c573aaa8e8913cae1653f1150
Matthew Barber <quitesimplymatt@gmail.com>
2020-06-11 09:36:32 +0100

    parameterizing checks for func name, instead of func object

--- tests/stattests/conftest.py
+++ tests/stattests/conftest.py
@@ -1,13 +1,5 @@
 from .test_examples import Example
 from .test_examples import iterexamples
-from .test_examples import test_stattest_on_example
-
-__all__ = ["parametrize_example_tests"]
-
-
-def parametrize_example_tests(metafunc):
-    title_substr = metafunc.config.getoption("example")
-    metafunc.parametrize(Example._fields, iterexamples(title_substr))
 
 
 def pytest_addoption(parser):
@@ -17,5 +9,6 @@ def pytest_addoption(parser):
 
 
 def pytest_generate_tests(metafunc):
-    if metafunc.function is test_stattest_on_example:
-        parametrize_example_tests(metafunc)
+    if metafunc.function.__name__ == "test_stattest_on_example":
+        title_substr = metafunc.config.getoption("example")
+        metafunc.parametrize(Example._fields, iterexamples(title_substr))
--- tests/stattests/implementations/conftest.py
+++
@@ -1,7 +0,0 @@
-from ..conftest import parametrize_example_tests
-from .test_dj import test_stattest_on_example
-
-
-def pytest_generate_tests(metafunc):
-    if metafunc.function is test_stattest_on_example:
-        parametrize_example_tests(metafunc)
