6a01b5fd68cd8d8bac87e7707ba1c198e311209a
Matthew Barber <quitesimplymatt@gmail.com>
2020-06-19 12:39:42 +0100

    better naming and structure for universal test

--- src/rngtest/stattests/_universal.py
+++ src/rngtest/stattests/_universal.py
@@ -2,14 +2,10 @@ from collections import defaultdict
 from math import erfc
 from math import log
 from math import sqrt
-from typing import Any
-from typing import Iterable
-from typing import Tuple
-
-import pandas as pd
+from typing import NamedTuple
 
 from rngtest.stattests._common import TestResult
-from rngtest.stattests._common import chunks
+from rngtest.stattests._common import rawchunks
 from rngtest.stattests._common import stattest
 
 __all__ = ["maurers_universal"]
@@ -20,62 +16,58 @@ __all__ = ["maurers_universal"]
 def maurers_universal(series, blocksize, init_nblocks):
     n = len(series)
     init_n = init_nblocks * blocksize
-    init_series, remaining_series = series[:init_n], series[init_n:]
-    remaining_nblocks = (n - init_n) / blocksize
+    init_series, spare_series = series[:init_n], series[init_n:]
+    spare_nblocks = (n - init_n) / blocksize
 
-    last_permutation_occurences = defaultdict(int)
+    last_occurences = defaultdict(int)
 
-    init_blocks = hashable_chunks(init_series, blocksize=blocksize)
-    for i, permutation in enumerate(init_blocks, 1):
-        last_permutation_occurences[permutation] = i
+    init_blocks = rawchunks(init_series, blocksize=blocksize)
+    for pos, permutation in enumerate(init_blocks, 1):
+        last_occurences[permutation] = pos
 
-    remaining_blocks = hashable_chunks(remaining_series, blocksize=blocksize)
-    cumulative_distances = 0
-    for i, permutation in enumerate(remaining_blocks, init_nblocks + 1):
-        last_occurence = last_permutation_occurences[permutation]
-        distance = i - last_occurence
-        cumulative_distances += log(distance, 2)
+    spare_blocks = rawchunks(spare_series, blocksize=blocksize)
+    spare_firstpos = init_nblocks + 1
+    distances_total = 0
+    for pos, permutation in enumerate(spare_blocks, spare_firstpos):
+        last_occurence = last_occurences[permutation]
+        distance = pos - last_occurence
+        distances_total += log(distance, 2)
 
-        last_permutation_occurences[permutation] = i
+        last_occurences[permutation] = pos
 
-    statistic = cumulative_distances / remaining_nblocks
-
-    stuff = hard_coded_thing.loc[hard_coded_thing["blocksize"] == blocksize]
-
-    p = erfc(
-        abs(
-            (statistic - stuff.expected_value.item())
-            / (sqrt(2) * stuff.variance.item())
-        )
-    )
+    statistic = distances_total / spare_nblocks
+    expected_mean, variance = blocksize_dists[blocksize]
+    p = erfc(abs((statistic - expected_mean) / (sqrt(2) * variance)))
 
     return TestResult(statistic=statistic, p=p)
 
 
+class Dist(NamedTuple):
+    mean: float
+    variance: float
+
+
+# Values taken from "A Handbook of Applied Cryptography"
+#     See section 5.4.5, "Maurer's universal statistical test", p. 184
 # TODO generate this dynamically
-hard_coded_thing = pd.DataFrame(
-    [
-        [2, 1.5374383, 1.338],
-        [6, 5.2177052, 2.954],
-        [7, 6.1962507, 3.125],
-        [8, 7.1836656, 3.238],
-        [9, 8.1764248, 3.311],
-        [10, 9.1723243, 3.356],
-        [11, 10.170032, 3.384],
-        [12, 11.168765, 3.401],
-        [13, 12.168070, 3.410],
-        [14, 13.167693, 3.416],
-        [15, 14.167488, 3.419],
-        [16, 15.167379, 3.421],
-    ],
-    columns=["blocksize", "expected_value", "variance"],
-)
-
-
-def hashable_chunks(*args, **kwargs) -> Iterable[Tuple[Any]]:
-    for chunk in chunks(*args, **kwargs):
-        yield tuple(chunk.tolist())
-
+blocksize_dists = {
+    1: Dist(0.7326495, 0.690),
+    2: Dist(1.5374383, 1.338),
+    3: Dist(2.4016068, 1.901),
+    4: Dist(3.3112247, 2.358),
+    5: Dist(4.2534266, 2.705),
+    6: Dist(5.2177052, 2.954),
+    7: Dist(6.1962507, 3.125),
+    8: Dist(7.1836656, 3.238),
+    9: Dist(8.1764248, 3.311),
+    10: Dist(9.1723243, 3.356),
+    11: Dist(10.170032, 3.384),
+    12: Dist(11.168765, 3.401),
+    13: Dist(12.168070, 3.410),
+    14: Dist(13.167693, 3.416),
+    15: Dist(14.167488, 3.419),
+    16: Dist(15.167379, 3.421),
+}
 
 # TODO default parameters using NIST's recommendations
 # n blocksize init_nblocks
