79a370371d3e570284be770e2ebb7ee02bdedf36
Matthew Barber <quitesimplymatt@gmail.com>
2020-06-25 14:56:18 +0100

    generators produce 0 or 1 ints, not bools

--- src/rngtest/generators.py
+++ src/rngtest/generators.py
@@ -1,35 +1,31 @@
-import random
 from math import sqrt
+from random import getrandbits
 
 __all__ = ["primes", "python"]
 
 
 def python():
     while True:
-        yield random.choice([True, False])
+        yield getrandbits(1)
 
 
-def natural_numbers():
-    n = 1
-
+def numbers(start=1):
+    n = start
     while True:
         yield n
         n += 1
 
 
-def is_prime(n):
-    if n == 2:
-        return True
-    if n % 2 == 0 or n <= 1:
-        return False
-
-    for divisor in range(3, int(sqrt(n)) + 1, 2):
-        if n % divisor == 0:
-            return False
-
-    return True
-
-
 def primes():
-    for n in natural_numbers():
-        yield is_prime(n)
+    yield 0  # n=1
+    yield 1  # n=2
+
+    for n in numbers(3):
+        if n % 2 == 0:
+            yield 0
+        else:
+            for divisor in range(3, int(sqrt(n)) + 1, 2):
+                if n % divisor == 0:
+                    yield 0
+            else:
+                yield 1
