2ce55aea151e4d9e95650acf0043d03f25b8b155
Matthew Barber <quitesimplymatt@gmail.com>
2020-07-03 14:20:16 +0100

    FloorDict doc

--- src/rngtest/stattests/_collections.py
+++ src/rngtest/stattests/_collections.py
@@ -3,7 +3,14 @@ from collections import UserDict
 __all__ = ["FloorDict"]
 
 
+# TODO ensure key order ala OrderedDic
 class FloorDict(UserDict):
+    """Dict where invalid keys floor to the smallest real key
+
+    If `__getitem__` is passed a non-existent key, `FloorDict` attempts to find
+    a real key that is the nearest less-than of the passed key.
+    """
+
     def __getitem__(self, key):
         try:
             return self.data[key]
