5f742cc01ea74263d2e75d4d6946261eeab00d75
Matthew Barber <quitesimplymatt@gmail.com>
2020-07-06 11:30:40 +0100

    testutils docs

--- src/rngtest/stattests/_testutils.py
+++ src/rngtest/stattests/_testutils.py
@@ -1,3 +1,4 @@
+"""Utility methods for randomness tests."""
 from math import ceil
 from typing import Any
 from typing import Dict
@@ -11,10 +12,33 @@ __all__ = ["blocks", "rawblocks", "check_recommendations"]
 
 
 def blocks(series, blocksize=None, nblocks=None, cutoff=True) -> Iterable[pd.Series]:
+    """Chunking method for `Series` objects
+
+    Parameters
+    ----------
+    series : `Series`
+        The pandas `Series` to chunk
+    blocksize : `int`, required if no `nblocks` passed
+        Size of the chunks
+    nblocks : `int`, required if no `blocksize` passed
+        Number of chunks
+    cutoff : `bool`, default `True`
+        Whether to discard remaning series
+
+    Yields
+    ------
+    block : `Series`
+        Chunk of the passed `series`
+
+    Raises
+    ------
+    ValueError
+        When neither `blocksize` or `nblocks` is passed
+    """
     n = len(series)
 
     if not blocksize and not nblocks:
-        raise ValueError()
+        raise ValueError("Either blocksize or nblocks must be specified")
     elif nblocks is None:
         nblocks = n // blocksize
     elif blocksize is None:
@@ -30,6 +54,30 @@ def blocks(series, blocksize=None, nblocks=None, cutoff=True) -> Iterable[pd.Ser
 
 
 def rawblocks(*args, **kwargs) -> Iterable[Tuple[Any]]:
+    """Tuple chunking method for `Series` objects
+
+    Parameters
+    ----------
+    *args
+        Positional arguments to pass to `blocks`
+    **kwargs
+        Keyword arguments to pass to `blocks`
+
+    Yields
+    ------
+    block_tup : `Tuple`
+        Tuple representation of the block
+
+    Raises
+    ------
+    ValueError
+        When neither `blocksize` or `nblocks` is passed
+
+    See Also
+    --------
+    blocks: The method `rawblocks` adapts
+    """
+
     for block in blocks(*args, **kwargs):
         block_list = block.tolist()
         block_tup = tuple(block_list)
@@ -38,6 +86,19 @@ def rawblocks(*args, **kwargs) -> Iterable[Tuple[Any]]:
 
 
 def check_recommendations(recommendation: Dict[str, bool]):
+    """Warns on recommendation failures
+
+    Parameters
+    ----------
+    recommendations : `Dict[str, bool]`
+        Map of recommendation string representations to the actual
+        recommendation outcomes
+
+    Warns
+    -----
+    UserWarning
+        When one or more recommendations fail
+    """
     failures = [expr for expr, success in recommendation.items() if not success]
 
     nfail = len(failures)
@@ -47,5 +108,5 @@ def check_recommendations(recommendation: Dict[str, bool]):
 
     elif nfail > 1:
         msg = "Multiple NIST recommendations not met:\n"
-        msg += "\n".join([f"     • {expr}" for expr in failures])
+        msg += "\n".join([f"• {expr}" for expr in failures])
         warn(msg, UserWarning)
