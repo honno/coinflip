6b3ab5aa266c944251fa7b419eb5fa7d7d70c901
Matthew Barber <quitesimplymatt@gmail.com>
2020-07-20 10:24:03 +0100

    contains_multiple_values -> is_multivalued, type hint return

--- tests/randtests/strategies.py
+++ tests/randtests/strategies.py
@@ -4,7 +4,7 @@ from hypothesis.strategies import SearchStrategy
 __all__ = ["mixedbits"]
 
 
-def contains_multiple_values(array):
+def is_multivalued(array) -> bool:
     firstval = array[0]
     for val in array[1:]:
         if val != firstval:
@@ -17,6 +17,6 @@ def mixedbits(min_size=2) -> SearchStrategy[int]:
     """Strategy to generate binary sequences"""
     binary = st.integers(min_value=0, max_value=1)
     bits = st.lists(binary, min_size=min_size)
-    mixedbits = bits.filter(contains_multiple_values)
+    mixedbits = bits.filter(is_multivalued)
 
     return mixedbits
