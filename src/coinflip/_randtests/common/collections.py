58ddf6ae63b8f67c2f434627eb323e9223681b56
Matthew Barber <quitesimplymatt@gmail.com>
2020-11-27 18:34:53 +0000

    working defaultlist, test against other impl

--- src/coinflip/_randtests/common/collections.py
+++ src/coinflip/_randtests/common/collections.py
@@ -5,6 +5,8 @@ from collections.abc import Mapping
 from collections.abc import MutableMapping
 from collections.abc import MutableSequence
 from functools import lru_cache
+from itertools import chain
+from itertools import count
 from numbers import Real
 from typing import Any
 from typing import Callable
@@ -13,6 +15,7 @@ from typing import Iterable
 from typing import Optional
 from typing import Sequence
 from typing import Tuple
+from typing import Type
 from typing import Union
 
 from sortedcontainers import SortedDict
@@ -126,7 +129,11 @@ class defaultlist(MutableSequence):
     """
 
     def __init__(self, default_factory: Optional[Callable] = None):
-        self._ddict = defaultdict(default_factory)
+        self._ddict = defaultdict(default_factory or defaultlist._none_factory)
+
+    @staticmethod
+    def _none_factory() -> None:
+        return None
 
     @property
     def default_factory(self) -> Optional[Callable]:
@@ -134,94 +141,136 @@ class defaultlist(MutableSequence):
 
     def __getitem__(self, key: Union[int, slice]):
         if isinstance(key, int):
-            # TODO neg
-            i = key if key >= 0 else len(self) + key
+            i = self._actualise_index(key)
             return self._ddict[i]
 
         elif isinstance(key, slice):
-            copy_range = range(len(self))[key]
-
+            srange = self._determine_srange(key)
             dlist = defaultlist()
-            dlist += [self._ddict[i] for i in copy_range]
-
+            dlist += [self._ddict[i] for i in srange]
             return dlist
 
         else:
-            name = type(key).__name__
-            raise TypeError(
-                f"defaultlist indices must be integers or slices, not {name}"
-            )
+            defaultlist._raise_type_error(type(key))
 
     def __setitem__(self, key: Union[int, slice], value: Any):
         if isinstance(key, int):
-            # TODO neg
-            i = key if key >= 0 else len(self) + key
+            i = self._actualise_index(key)
             self._ddict[i] = value
 
         elif isinstance(key, slice):
-            n = len(self)
-
             values = list(value) if isinstance(value, Iterable) else [value]
             nvalues = len(values)
 
-            # 0 determine slice range
+            srange = self._determine_srange(key)
+            if srange:
+                srange_min = min(srange[0], srange[-1])
+                srange_max = max(srange[0], srange[-1])
+            else:
+                srange_min = srange_max = srange.start
 
-            start, stop, step = key.indices(n)
+            for i in srange:
+                try:
+                    del self._ddict[i]
+                except KeyError:
+                    pass
 
-            # 1.1 determine del range
+            diff = nvalues - len(srange)
+            if diff:
+                keys = list(self._ddict.keys())
 
-            dstart, dstop = start, stop
+                threshold = srange_min + nvalues
+                reindexed_subdict = {}
 
+                mid_keys = [k for k in keys if srange_min <= k < srange_max]
+                for i, k in zip(count(threshold), sorted(mid_keys)):
+                    reindexed_subdict[i] = self._ddict[k]
+
+                larger_keys = [k for k in self._ddict.keys() if k > srange_max]
+                for k in larger_keys:
+                    i = k + diff
+                    reindexed_subdict[i] = self._ddict[k]
+
+                self._ddict.update(reindexed_subdict)
+
+            for i, v in zip(count(srange_min), values):
+                self[i] = v
+
+        else:
+            defaultlist._raise_type_error(type(key))
+
+    def __delitem__(self, key: Union[int, slice]):
+        if isinstance(key, int):
+            i = self._actualise_index(key)
             try:
-                if (dstop - dstart) / step < 0:  # diverging
-                    if step >= 1:
-                        dstop = dstart
-                    else:
-                        dstart = dstop
-            except ZeroDivisionError:
+                del self._ddict[i]
+            except KeyError:
                 pass
 
-            del_range = range(dstart, dstop, step)
+            larger_keys = [k for k in self._ddict.keys() if k > i]
+            reindexed_subdict = {k - 1: self._ddict[k] for k in larger_keys}
+            for k in larger_keys:
+                del self._ddict[k]
+            self._ddict.update(reindexed_subdict)
 
-            # 1.2 determine insert range
+        elif isinstance(key, slice):
+            srange = self._determine_srange(key)
+            if srange:
+                for i in srange:
+                    try:
+                        del self._ddict[i]
+                    except KeyError:
+                        pass
 
-            istop = start + step * nvalues
-            insert_range = range(start, istop, step)
+                srange_min = min(srange[0], srange[-1])
+                srange_max = max(srange[0], srange[-1])
 
-            # 2. delete keys in del range
+                reindexed_subdict = {}
 
-            for i in [k for k in self._ddict.keys() if k in del_range]:
-                del self._ddict[i]
-
-            # 3. update keys above del range
-
-            diff = nvalues - len(del_range)
-            if diff:
-                larger_keys = [
-                    k
-                    for k in self._ddict.keys()
-                    if k >= max(del_range.start, del_range.stop)
+                mid_keys = [
+                    k for k in self._ddict.keys() if srange_min <= k < srange_max
                 ]
-                reindexed_subdict = {k + diff: self._ddict[k] for k in larger_keys}
+                for i, k in zip(count(srange_min), sorted(mid_keys)):
+                    reindexed_subdict[i] = self._ddict[k]
 
+                larger_keys = [k for k in self._ddict.keys() if k > srange_max]
                 for k in larger_keys:
+                    i = k - len(srange)
+                    reindexed_subdict[i] = self._ddict[k]
+
+                for k in chain(mid_keys, larger_keys):
                     del self._ddict[k]
                 self._ddict.update(reindexed_subdict)
 
-            # 4. insert values safely
-
-            for k, v in zip(insert_range, values):
-                self._ddict[k] = v
-
         else:
-            name = type(key).__name__
-            raise TypeError(
-                f"defaultlist indices must be integers or slices, not {name}"
-            )
+            defaultlist._raise_type_error(type(key))
 
-    # TODO support slices
-    def __delitem__(self, i: int):
-        del self._ddict[i]
+    def _actualise_index(self, key: int) -> int:
+        if key >= 0:
+            return key
+        else:
+            n = len(self)
+            if key >= -n:
+                return n + key
+            else:
+                raise IndexError(
+                    "negative list index larger than list length, unresolvable"
+                )
+
+    def _determine_srange(self, slice_: slice) -> range:
+        step = slice_.step or 1
+
+        if slice_.start is None:
+            start = 0 if step > 0 else len(self)
+        else:
+            start = self._actualise_index(slice_.start)
+
+        if slice_.stop is None:
+            stop = len(self) if step > 0 else 0
+        else:
+            stop = self._actualise_index(slice_.stop)
+
+        return range(start, stop, step)
 
     def __len__(self):
         if self._ddict.keys():
@@ -257,6 +306,11 @@ class defaultlist(MutableSequence):
 
     def __str__(self):
         return f"defaultlist({self.default_factory}, {repr(self)})"
+
+    @staticmethod
+    def _raise_type_error(type_: Type):
+        name = type_.__name__
+        raise TypeError(f"defaultlist indices must be integers or slices, not {name}")
 
 
 class FloorDict(Mapping):
--- tests/collections/test_defaultlist.py
+++ tests/collections/test_defaultlist.py
@@ -1,9 +1,10 @@
+from defaultlist import defaultlist as ref_defaultlist
 from hypothesis import assume
-from hypothesis import reject
 from hypothesis import strategies as st
 from hypothesis.stateful import RuleBasedStateMachine
 from hypothesis.stateful import initialize
 from hypothesis.stateful import rule
+from pytest import fixture
 
 from coinflip.collections import defaultlist
 
@@ -11,78 +12,95 @@ ints = st.lists(st.integers())
 chars = st.from_regex(r"[a-z]*", fullmatch=True)
 
 
-# TODO test extended slices (ugh)
+# we disagree about slices so we use no rules for slice accessors
 class DefaultListStateMachine(RuleBasedStateMachine):
     @initialize(ints=ints)
     def init_lists(self, ints):
-        self.list_ = ints
-
+        self.ref_dlist = ref_defaultlist()
         self.dlist = defaultlist()
-        self.dlist[:] = ints
+
+        assert self.dlist == self.ref_dlist
 
     @property
     def n(self):
-        return len(self.list_)
+        return len(self.ref_dlist)
+
+    @rule(chars=chars)
+    def append(self, chars):
+        self.ref_dlist.append(chars)
+        self.dlist.append(chars)
+
+        assert self.dlist == self.ref_dlist
+
+    @rule(ints=ints)
+    def concat(self, ints):
+        self.ref_dlist += ints
+        self.dlist += ints
+
+        assert self.dlist == self.ref_dlist
 
     @rule(data=st.data())
     def get(self, data):
         assume(self.n > 0)
         i = data.draw(st.integers(min_value=0, max_value=self.n - 1))
 
-        assert self.dlist[i] == self.list_[i]
+        assert self.dlist[i] == self.ref_dlist[i]
 
     @rule(data=st.data(), chars=chars)
     def set(self, data, chars):
         assume(self.n > 0)
         i = data.draw(st.integers(min_value=0, max_value=self.n - 1))
 
-        self.list_[i] = chars
+        self.ref_dlist[i] = chars
         self.dlist[i] = chars
 
-        assert self.dlist == self.list_
-
-    @rule(chars=chars)
-    def append(self, chars):
-        self.list_.append(chars)
-        self.dlist.append(chars)
-
-        assert self.dlist == self.list_
-
-    @rule(ints=ints)
-    def concat(self, ints):
-        self.list_ += ints
-        self.dlist += ints
-
-        assert self.dlist == self.list_
+        assert self.dlist == self.ref_dlist
 
     @rule(data=st.data())
-    def slice_get(self, data):
-        slice_ = data.draw(st.slices(self.n))
+    def del_(self, data):
+        assume(self.n > 0)
+        i = data.draw(st.integers(min_value=0, max_value=self.n - 1))
 
-        assert self.dlist[slice_] == self.list_[slice_]
+        del self.ref_dlist[i]
+        del self.dlist[i]
 
-    @rule(data=st.data(), chars_or_ints=st.one_of(chars, ints))
-    def slice_set(self, data, chars_or_ints):
-        slice_ = data.draw(st.slices(self.n))
-
-        try:
-            self.list_[slice_] = chars_or_ints
-        except ValueError:
-            reject()
-        self.dlist[slice_] = chars_or_ints
-
-        assert self.dlist == self.list_
+        assert self.dlist == self.ref_dlist
 
 
 TestDefaultListStateMachine = DefaultListStateMachine.TestCase
 
 
-def test_repr():
-    dlist = defaultlist(int)
+@fixture
+def dlist():
+    return defaultlist()
+
+
+def test_repr(dlist):
     dlist.append(42)
     assert repr(dlist) == "[42]"
 
 
-def test_slice_defaulting():
-    dlist = defaultlist(int)
-    assert dlist[:2] == [0, 0]
+def test_slice_del(dlist):
+    dlist[:] = range(15)
+    dlist[20] = 20
+
+    del dlist[0:10:2]
+
+    assert dlist == [
+        1,
+        3,
+        5,
+        7,
+        9,
+        10,
+        11,
+        12,
+        13,
+        14,
+        None,
+        None,
+        None,
+        None,
+        None,
+        20,
+    ]
