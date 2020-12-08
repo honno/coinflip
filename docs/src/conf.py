4bf5d14f89dfc2773e8d6557638c2f90a844a37e
Matthew Barber <quitesimplymatt@gmail.com>
2020-12-08 11:35:49 +0000

    Bump version: 0.1.4 → 0.1.5

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -18,7 +18,7 @@ project = "coinflip"
 year = "2020"
 author = "Matthew Barber"
 copyright = "{0}, {1}".format(year, author)
-version = release = "0.1.4"
+version = release = "0.1.5"
 
 pygments_style = "trac"
 templates_path = ["."]
