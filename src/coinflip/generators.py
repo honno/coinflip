697617ca8b2c3fe1a2435d4838b7610fa421c8f6
Matthew Barber <quitesimplymatt@gmail.com>
2021-02-15 10:04:43 +0000

    mersenne twister generator

--- src/coinflip/generators.py
+++ src/coinflip/generators.py
@@ -4,7 +4,7 @@ Objects infinitely generate ``0`` and ``1`` integers to represent a binary seque
 from collections.abc import Iterator
 from random import Random
 
-__all__ = ["Python"]
+__all__ = ["Python", "Mersenne"]
 
 
 class Python(Iterator):
@@ -13,3 +13,18 @@ class Python(Iterator):
 
     def __next__(self):
         return self._rng.getrandbits(1)
+
+
+class Mersenne(Iterator):
+    def __init__(self, seed=42):
+        self.seed = seed
+        self.j = 2 ** 31 - 1
+        self.k = 16807
+        self.period = 2 ** 30
+
+    def __next__(self):
+        self.seed = (self.k * self.seed) % self.j
+        if self.seed / self.j < 0.50:
+            return 0
+        else:
+            return 1
